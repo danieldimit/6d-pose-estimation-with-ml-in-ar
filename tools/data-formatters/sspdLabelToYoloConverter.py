75a5ed6174977b8243f8bae43bb4f9d00e17eaa1
danidim <dani2dim@yahoo.com>
2019-06-23 19:42:46 +0200

    Added an sspd lbl to yolo lbl converter

---
+++ tools/data-formatters/sspdLabelToYoloConverter.py
@@ -0,0 +1,41 @@
+import glob, os, shutil
+
+if not os.path.exists('./converted'):
+    os.makedirs('./converted')
+
+os.chdir('./labels')
+for file in glob.glob("*.txt"):
+    f = open(file, "r")
+    line = f.read()
+
+    lineVals = line.split()
+
+    newLine = lineVals[0] + ' ' + lineVals[1] + ' ' + lineVals[2] + ' ' + lineVals[19] + ' ' + lineVals[20]
+    with open('../converted/' + file, 'w') as file:
+        file.write(newLine)
+os.chdir('../')
+
+# delete all files in labels
+shutil.rmtree('./labels')
+
+# move converted to labels
+os.rename('./converted', './labels')
+
+# fix train and test
+cwd = os.getcwd()
+with open('./train_new.txt', 'w') as file:
+    f = open('./train.txt', "r")
+    for x in f:
+        file.write(x.replace('sspdFormat', cwd).replace('g ', 'g'))
+    f.close()
+
+with open('./test_new.txt', 'w') as file:
+    f = open('./test.txt', "r")
+    for x in f:
+        file.write(x.replace('sspdFormat', cwd).replace('g ', 'g'))
+    f.close()
+
+os.remove('./train.txt')
+os.remove('./test.txt')
+os.rename('./train_new.txt', './train.txt')
+os.rename('./test_new.txt', './test.txt')
