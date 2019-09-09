12ef51f83896e338ab0b884fd9bfedbaf5002c49
danidim <dani2dim@yahoo.com>
2019-09-09 05:01:25 +0200

    Added point cloud projection on labels

--- tools/projectors/3d 2d bounding box betapose/labels/kpd/project_single.py
+++ tools/projectors/3d 2d bounding box betapose/labels/kpd/project_single.py
@@ -6,6 +6,7 @@ import random
 import yaml
 import cv2
 from utils import *
+from MeshPly import MeshPly
 from distutils.dir_util import copy_tree
 
 def splitAndCastToFloat(line):
@@ -15,80 +16,60 @@ def splitAndCastToFloat(line):
 
 
 def getBBOfPlyObject(gt_folder, img_num, ply_name):
+	mesh          = MeshPly(gt_folder + '/' + ply_name)
+	vertices      = np.c_[np.array(mesh.vertices), np.ones((len(mesh.vertices), 1))].transpose()
+	corners3D     = get_3D_corners(vertices)
 
-	with open(gt_folder + '/' + ply_name) as f:
-		content = f.readlines()
-		content = [x.strip() for x in content] 
+
+	with open(gt_folder + "/gt.yml", 'r') as stream:
+		try:
+			img_infos = yaml.safe_load(stream)[img_num][0]
+			R = np.array(img_infos['cam_R_m2c']).reshape(3, 3)
+			Rt = np.append(R, np.array([img_infos['cam_t_m2c']]).T, axis=1)
+			np.set_printoptions(suppress=True)
+			with open(gt_folder + "/info.yml", 'r') as info_stream:
+				try:
+					img_infos = yaml.safe_load(info_stream)[img_num]
+					i_c = np.array(img_infos['cam_K']).reshape(3, 3)
+				except yaml.YAMLError as exc:
+					print(exc)
+		except yaml.YAMLError as exc:
+			print(exc)
 		
-		skip = 0
-		foundVertexEl = False
-		foundEndOfHead = False
-		lineVals = []
-		linesToScan = 0
+	proj_2d_p   = compute_projection(corners3D, Rt, i_c)
+	proj_2d_p = proj_2d_p.astype(int)
 
-		while (not foundVertexEl or not foundEndOfHead):
-			lineVals = content[skip].split()
-			if (lineVals[0] == 'end_header'):
-				foundEndOfHead = True
-			if (lineVals[0] == 'element'):
-				if (lineVals[1] == 'vertex'):
-					linesToScan = int(lineVals[2])
-					foundVertexEl = True
-			skip+=1
-		content = content[skip:linesToScan+skip]
-		copy = [];
-		for line in content: 
-			copy.append(splitAndCastToFloat(line))
-		vertices = np.matrix(np.array(copy))
-		mins = vertices.min(0)
-		maxs = vertices.max(0)
-		minsMaxs = np.array([[mins.item(0),mins.item(1),mins.item(2)], [maxs.item(0),maxs.item(1),maxs.item(2)]]).T
-		corners = np.array(np.meshgrid(minsMaxs[0,:], minsMaxs[1,:], minsMaxs[2,:])).T.reshape(-1,3)
+	proj_2d_vert = compute_projection(vertices, Rt, i_c).astype(int).T
 
-		corners = np.c_[corners, np.ones((len(corners), 1))].transpose()
 
-		with open(gt_folder + "/gt.yml", 'r') as stream:
-			try:
-				img_infos = yaml.safe_load(stream)[img_num][0]
-				R = np.array(img_infos['cam_R_m2c']).reshape(3, 3)
-				Rt = np.append(R, np.array([img_infos['cam_t_m2c']]).T, axis=1)
-				np.set_printoptions(suppress=True)
-				with open(gt_folder + "/info.yml", 'r') as info_stream:
-					try:
-						img_infos = yaml.safe_load(info_stream)[img_num]
-						i_c = np.array(img_infos['cam_K']).reshape(3, 3)
-					except yaml.YAMLError as exc:
-						print(exc)
-			except yaml.YAMLError as exc:
-				print(exc)
-			
-		proj_2d_p   = compute_projection(corners, Rt, i_c)
-		proj_2d_p = proj_2d_p.astype(int)
+	# Make empty black image
+	image=cv2.imread(gt_folder + '/rgb/' + format(img_num, '04') + '.png',1)
+	height, width, channels = image.shape
+	blue = [255,0,0]
+	image[proj_2d_p[1,:],proj_2d_p[0,:]]=blue
 
-		# Make empty black image
-		image=cv2.imread(gt_folder + '/rgb/' + format(img_num, '04') + '.png',1)
-		height, width, channels = image.shape
-		blue = [255,0,0]
-		image[proj_2d_p[1,:],proj_2d_p[0,:]]=blue
+	for c in proj_2d_vert:
+		if (c[0] < width and c[0] >= 0 and c[1] < height and c[1] >= 0):
+			image[c[1], c[0]]=(0,255,0)
 
-		# Draw lower base of 3d bb
-		pts = np.array([[proj_2d_p[0,0], proj_2d_p[1,0]],[proj_2d_p[0,2], proj_2d_p[1,2]],[proj_2d_p[0,3], proj_2d_p[1,3]],[proj_2d_p[0,1], proj_2d_p[1,1]]], np.int32)
-		cv2.polylines(image,[pts],True,(255,0,0))
-		
-		# Draw the front of the bounding box
-		pts = np.array([[proj_2d_p[0,3], proj_2d_p[1,3]],[proj_2d_p[0,7], proj_2d_p[1,7]],[proj_2d_p[0,6], proj_2d_p[1,6]],[proj_2d_p[0,2], proj_2d_p[1,2]]], np.int32)
-		cv2.polylines(image,[pts],True,(0,0,255))
+	# Draw lower base of 3d bb
+	pts = np.array([[proj_2d_p[0,0], proj_2d_p[1,0]],[proj_2d_p[0,2], proj_2d_p[1,2]],[proj_2d_p[0,3], proj_2d_p[1,3]],[proj_2d_p[0,1], proj_2d_p[1,1]]], np.int32)
+	cv2.polylines(image,[pts],True,(255,0,0))
+	
+	# Draw the front of the bounding box
+	pts = np.array([[proj_2d_p[0,3], proj_2d_p[1,3]],[proj_2d_p[0,7], proj_2d_p[1,7]],[proj_2d_p[0,6], proj_2d_p[1,6]],[proj_2d_p[0,2], proj_2d_p[1,2]]], np.int32)
+	cv2.polylines(image,[pts],True,(0,0,255))
 
-		# Draw upper base of 3d bb
-		pts = np.array([[proj_2d_p[0,4], proj_2d_p[1,4]],[proj_2d_p[0,6], proj_2d_p[1,6]],[proj_2d_p[0,7], proj_2d_p[1,7]],[proj_2d_p[0,5], proj_2d_p[1,5]]], np.int32)
-		cv2.polylines(image,[pts],True,(0,255,255))
+	# Draw upper base of 3d bb
+	pts = np.array([[proj_2d_p[0,4], proj_2d_p[1,4]],[proj_2d_p[0,6], proj_2d_p[1,6]],[proj_2d_p[0,7], proj_2d_p[1,7]],[proj_2d_p[0,5], proj_2d_p[1,5]]], np.int32)
+	cv2.polylines(image,[pts],True,(0,255,255))
 
-		# Draw -y side
-		pts = np.array([[proj_2d_p[0,0], proj_2d_p[1,0]],[proj_2d_p[0,2], proj_2d_p[1,2]],[proj_2d_p[0,6], proj_2d_p[1,6]],[proj_2d_p[0,4], proj_2d_p[1,4]]], np.int32)
-		cv2.polylines(image,[pts],True,(0,255,0))
+	# Draw -y side
+	pts = np.array([[proj_2d_p[0,0], proj_2d_p[1,0]],[proj_2d_p[0,2], proj_2d_p[1,2]],[proj_2d_p[0,6], proj_2d_p[1,6]],[proj_2d_p[0,4], proj_2d_p[1,4]]], np.int32)
+	cv2.polylines(image,[pts],True,(0,255,0))
 
-		# Save
-		cv2.imwrite("result.png",image)
+	# Save
+	cv2.imwrite("result.png",image)
 		
 
 if __name__ == "__main__":
